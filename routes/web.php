aa0ea4393c79c1d4463b8517c9dee89a3e66ac05
DevEingel <70227293+sinihes@users.noreply.github.com>
2022-08-17 18:39:25 +0200

    clase-rutas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/test', function () {
+    echo "<h1>Hola mundo</h1>";
+});
+
